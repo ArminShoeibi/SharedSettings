024681626ee4c764e7f70b9fec5f257259df9874
Armin Shoeibi <a.shoeibi@alibaba.ir>
2022-12-10 03:42:06 +0330

    Complete SharedSettings

--- src/SharedSettings.Shared/HostBuilderExtensions.cs
+++ src/SharedSettings.Shared/HostBuilderExtensions.cs
@@ -1,14 +1,17 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 
 namespace SharedSettings.Shared;
 public static class HostBuilderExtensions
 {
     public static IHostBuilder AddSharedSettings(this IHostBuilder hostBuilder)
     {
-        return hostBuilder.ConfigureAppConfiguration((hostBuilderContext, configurationBuilder) =>
+        // ConfigureAppConfiguration method can be called multiple times and  the results will be additive
+        return hostBuilder.ConfigureAppConfiguration((hostingContext, configurationBuilder) =>
         {
-            configurationBuilder.addj
-
+            string sharedSettingsFile = $"SharedSettings.{hostingContext.HostingEnvironment.EnvironmentName}.json";
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, sharedSettingsFile);
+            configurationBuilder.AddJsonFile(path, optional: true, reloadOnChange: true);
         });
     }
 }
