8dc64c5b8353bb420ba7ab93389e167278264cf8
Armin Shoeibi <a.shoeibi@alibaba.ir>
2022-12-10 03:42:25 +0330

    Invoke AddSharedSettings method

--- src/SharedSettings.WorkerService1/Program.cs
+++ src/SharedSettings.WorkerService1/Program.cs
@@ -1,6 +1,8 @@
+using SharedSettings.Shared;
 using SharedSettings.WorkerService1;
 
 IHost host = Host.CreateDefaultBuilder(args)
+    .AddSharedSettings()
     .ConfigureServices(services =>
     {
         services.AddHostedService<Worker>();
--- src/SharedSettings.WorkerService3/Program.cs
+++ src/SharedSettings.WorkerService3/Program.cs
@@ -1,10 +1,8 @@
+using SharedSettings.Shared;
 using SharedSettings.WorkerService3;
 
 IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureAppConfiguration(configurationBuilder => 
-    {
-        configurationBuilder.AddJsonFile();
-    })
+    .AddSharedSettings()
     .ConfigureServices(services =>
     {
         services.AddHostedService<Worker>();
