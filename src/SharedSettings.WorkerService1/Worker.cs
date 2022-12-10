f8593e72dc8cbd82d12b8b923baea623d77d146c
Armin Shoeibi <a.shoeibi@alibaba.ir>
2022-12-10 03:43:21 +0330

    Inject IConfiguration into workers to log SharedSetting

--- src/SharedSettings.WorkerService1/Worker.cs
+++ src/SharedSettings.WorkerService1/Worker.cs
@@ -3,17 +3,19 @@ namespace SharedSettings.WorkerService1;
 public class Worker : BackgroundService
 {
     private readonly ILogger<Worker> _logger;
+    private readonly IConfiguration _configuration;
 
-    public Worker(ILogger<Worker> logger)
+    public Worker(ILogger<Worker> logger, IConfiguration configuration)
     {
         _logger = logger;
+        _configuration = configuration;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         while (!stoppingToken.IsCancellationRequested)
         {
-            _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+            _logger.LogInformation("Worker running at: {time} - {Key1}", DateTimeOffset.Now, _configuration.GetValue<string>("Key1"));
             await Task.Delay(1000, stoppingToken);
         }
     }
